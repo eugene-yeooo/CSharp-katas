cd263622ace023914630bf4b3f2f2df217d5074d
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-09-06 12:38:10 +1200

    implement palindrome method normalising for punctuation and spaces

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,8 +1,9 @@
-﻿using Palindrome;
+﻿using System.Net.Http.Headers;
+using Palindrome;
 
 string[] testInputs = new string[]
 {
-  "racecar",
+    "racecar",
     "level",
     "hello",
     "world",
@@ -11,17 +12,32 @@ string[] testInputs = new string[]
     "noon",
     "palindrome",
     "anna",
+    "A man, a plan, a canal: Panama"
 };
 
 Console.WriteLine("Hello World!");
-Console.WriteLine("This is my Palindrome kata.");
+Console.WriteLine("This is my Palindrome kata using a Reverse method:");
 
 foreach (var word in testInputs)
 {
-  // Console.WriteLine($"{word} -> {PalindromeKata.ReverseInput(word)}");
+  Console.WriteLine($"{word} -> {PalindromeKata.ReverseInput(word)}");
+}
+
+Console.WriteLine(" ");
+Console.WriteLine("This palindrome checker uses a Two Point method:");
+
+foreach (var word in testInputs)
+{
   Console.WriteLine($"{word} -> {PalindromeKata.TwoPoint(word)}");
 }
 
+Console.WriteLine("");
+Console.WriteLine("This method normalizes for spaces and punctuation:");
+
+foreach (var input in testInputs)
+{
+  Console.WriteLine($"{input} -> {PalindromeKata.Normalize(input)}");
+}
 
 
 
@@ -51,6 +67,11 @@ namespace Palindrome
 
       return true;
     }
+    public static bool Normalize(string input)
+    {
+      var clean = new string(input.Where(char.IsLetterOrDigit).Select(char.ToLower).ToArray());
+
+      return clean == new string(clean.Reverse().ToArray());
+    }
   }
 }
-
