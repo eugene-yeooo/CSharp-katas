0cbada92dd74d6c0d6be54a3c8bc0093be644424
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-09-25 16:49:49 +1200

    create PrintBoard function

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,3 +7,17 @@ for (int row = 0; row < 3; row++)
     board[row, col] = ' ';
   }
 }
+
+void PrintBoard(char[,] board)
+{
+  for (int row = 0; row < 3; row++)
+  {
+    Console.WriteLine(" {0} | {1} | {2}", board[row, 0], board[row, 1], board[row, 2]);
+    if (row < 2)
+    {
+      Console.WriteLine("---+---+---");
+    }
+  }
+}
+
+PrintBoard(board);
